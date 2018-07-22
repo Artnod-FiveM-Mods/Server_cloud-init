afb18d2681bb78abcd77ecabe104cf2eb3021ef8
Artnod <artnod78@users.noreply.github.com>
2018-07-22 13:36:07 +0200

    Add files via upload

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-main() {
-	echo "Fivem user and group "
+defaultInstall() {
+	echo "  - *** Fivem user and group ***"
 	useradd -d /home/fivem -m -r -s /bin/bash -U fivem
 
 	if [ -e /home/fivem/fx.tar.xz ]; then
@@ -9,14 +9,14 @@ main() {
 		rm -fr /home/fivem/fx.tar.xz
 	fi
 
-	echo "  - Fivem Download"
+	echo "  - *** Fivem Download ***"
 	fvDlUrl="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/679-996150c95a1d251a5c0c7841ab2f0276878334f7/fx.tar.xz"
 	wget -nv -q --show-progress $fvDlUrl -O /home/fivem/fx.tar.xz
 	cd /home/fivem
 	tar xfJ fx.tar.xz
-	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server_cloud-init/master/start.sh -O /home/fivem/start.sh
-	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server_cloud-init/master/stop.sh -O /home/fivem/stop.sh
-	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server_cloud-init/master/fivem.service -O /etc/systemd/system/fivem.service
+	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server-cloud-init/master/start.sh -O /home/fivem/start.sh
+	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server-cloud-init/master/stop.sh -O /home/fivem/stop.sh
+	wget https://raw.githubusercontent.com/Artnod-FiveM-Mods/Server-cloud-init/master/fivem.service -O /etc/systemd/system/fivem.service
 
 	echo "  - Fivem public server-data"
 	git clone https://github.com/citizenfx/cfx-server-data.git /home/fivem/server-data
@@ -26,24 +26,48 @@ main() {
 	chmod -R 0775 /home/fivem/
 	chown root.root /etc/systemd/system/fivem.service
 	chmod 0777 /etc/systemd/system/fivem.service
-	chown root.root /etc/systemd/system/fivem.service
-	chmod 0777 /etc/systemd/system/fivem.service
 	
 	echo "  - FiveM Create deamon"
 	systemctl enable fivem.service
 	
 	echo "  - Script ended successfully. You can now:"
-	echo "    - Configure /home/fivem/server-data/server.cfg"
+	echo "    - Add /home/fivem/server-data/server.cfg"
 	echo "    - Add resources in /home/fivem/server-data/resources/"
 }
 
+customInstall() {
+	echo "  - *** Custon Init ***"
+	cp /etc/ssh/ssh_host_rsa_key /root/.ssh/id_rsa
+	cp /etc/ssh/ssh_host_rsa_key.pub /root/.ssh/id_rsa.pub
+	ssh-keygen -F github.com || ssh-keyscan github.com >> /root/.ssh/known_hosts
+	git config --global user.name "artnod78"
+	git config --global user.mail "artnod78@gmail.com"
+	
+	echo "  - *** Clone Custom repo ***"
+	git clone git@github.com:artnod78/Fivem-Server.git /root/fmm
+	
+	echo "  - *** Exec Custom Script ***"
+	sed -i -e 's/\r$//' /root/fmm/bootstrap.sh
+	chmod +x /root/fmm/bootstrap.sh
+	/root/fmm/bootstrap.sh -i
+	
+}
+
+main() {
+	if [ $RUNINSTALL -eq 1 ]; then
+		defaultInstall
+	fi
+	if [ $CUSTOMINSTALL -eq 1 ]; then
+		customInstall
+	fi
+}
 
 if [ -z $1 ]; then
 	echo "  - https://github.com/Artnod-FiveM-Mods/Server-cloud-init"
 	exit 0
 fi
 
-while getopts "hcoi" opt; do
+while getopts "hic" opt; do
 	case "$opt" in
 		h)
 			showHelp
@@ -52,8 +76,9 @@ while getopts "hcoi" opt; do
 		i)
 			RUNINSTALL=1
 			;;
+		c)
+			CUSTOMINSTALL=1
+			;;
 	esac
 done
-if [ $RUNINSTALL -eq 1 ]; then
-	main
-fi
+main
