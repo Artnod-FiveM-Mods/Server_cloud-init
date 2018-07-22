7f26dac1845061c6cf47f0f0f409611e4a5f6f4a
Artnod <artnod78@users.noreply.github.com>
2018-07-22 11:49:03 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+su - fivem -c "cd /home/fivem/server-data && /usr/bin/screen -dmS CitizenFx bash /home/fivem/run.sh +exec server.cfg"
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+su - fivem -c "/usr/bin/screen -S CitizenFx -X stuff $'\003'"
